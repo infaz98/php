00d8f5eef1f64e7cbf7b15b0a54272216cc814cf
Infaz <infaz.rumy@outlook.com>
2020-11-26 00:59:52 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Ruff</title>
+
+    	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto+Condensed:wght@300;700&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script></head>
+
+    <link rel="stylesheet" href="../css/index.css">
+
+</head>
+<body>
+    
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">PetRuff</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Log Out</a></li>
+    </ul>
+  </div>
+</nav>
+
+    <header>   
+        <div>
+            <h1>Pet Ruff</h1>
+            <h2>Your pets' favourite place, One stop for everything</h2>
+        </div>
+        
+     </header>
+
+
+     <section>
+        <div class="container">
+            <h3>Best place for all your pets needs, One stop for everything</h3>
+            <p>
+                All your pet need from food to health care for every need we bring the best quality products and service to your fingertip.
+            </p>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <div class="card">
+                <h3>Cat Food</h3>
+                <a href="../html/catFood.html"><img src="../images/cat_food.jpg" class="imgFood"></a>
+                <p>10% all cat food items</p>
+              </div>
+            </div>
+          
+            <div class="column">
+              <div class="card">
+                <h3>Dog Food</h3>
+                <a href="../html/dogFood.html"><img src="../images/dog_food.jpg" class="imgFood"></a>
+                <p>20% on selected items</p>
+            </div>
+            </div>
+            
+            <div class="column">
+              <div class="card">
+                <h3>Bird Food</h3>
+                <a href="../html/birdFood.html"><img src="../images/bird_food.jpg" class="imgFood"></a>
+                <p>Buy one Pack and get one free</p>
+              </div>
+            </div>
+            
+            <div class="column">
+              <div class="card">
+                <h3>Fish Food</h3>
+                <a href="../html/fishFood.html"><img src="../images/fish_food.jpg" class="imgFood"></a>
+                <p>Discounts over $20 purchases</p>
+              </div>
+            </div>
+          </div>
+     </section>
+
+     <footer>
+        <br>
+        <p>Author: Pudubu Dasun<br>
+        <a href="mailto:info@petruff.com">info@petruff.com</a></p>
+     </footer>
+    
+    </body>
+</html>
---
+++ php/customer.php
@@ -0,0 +1,38 @@
+<?php
+class Customer	 {
+
+	public $name;
+	public $color;
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>	
+ <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script></head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">PetRuff</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Log Out</a></li>
+    </ul>
+  </div>
+</nav>
+    </body>
+</html>
+
