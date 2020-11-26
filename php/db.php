6787d1865f8a5e8ee9bafb3d42ffcee605b05e37
Infaz <infaz.rumy@outlook.com>
2020-11-26 17:37:18 +0530

    added

--- php/db.php
+++ php/db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","PettRuff");
+$con = mysqli_connect("r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","wv31q6rsvi73js9d","e3zbjqh87pb82c9d","i0jpki45y7tukz6i");
 // Check connection
 if (mysqli_connect_errno())
   {
